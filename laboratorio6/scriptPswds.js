24fe1573a979572c574718bf166d30e58989b1e3
AnaSofiaMorenoA01707156 <A01707156@tec.mx>
2026-02-23 09:43:32 -0600

    Añadido de (archivos de)  ejercicio de laboratorio 6 ya completado.

--- laboratorio6/scriptPswds.js
+++ laboratorio6/scriptPswds.js
@@ -1,23 +1,33 @@
-const enviado = document.getElementById("boton");
+const enviado = document.getElementById("botonValidar");
 const forma = document.getElementById("forma");
 const inputPswd = document.getElementById("password");
-const campoVerif = document.getElementById("campov");
+const inputVerif = document.getElementById("verif");
+const reset = document.getElementById("botonReinicio");
+
+const limpiar = () => {
+    document.querySelectorAll(".inputWarning").forEach(el => el.remove());
+    forma.reset();
+}
 
 const validar = () => {
+    document.querySelectorAll(".inputWarning").forEach(el => el.remove());
     let pswd = forma["password"].value;
     let verif = forma["verif"].value;
     if (pswd == "") {
         inputPswd.insertAdjacentHTML("afterend", `<p class="inputWarning">Campo requerido.</p>`);
     }
     if (verif == "") {
-        alert("Se debe llenar el campo de verificar password.");
+        inputVerif.insertAdjacentHTML("afterend", `<p class="inputWarning">Campo requerido.</p>`);
     }
-    else if (pswd == verif){
-        forma.innerHTML = `<h2 id="aceptado" class="containerMarcado">¡Password nueva confirmada con éxito! (La contraseña coincide con su verificación).</h2>`;
-    }
-    else{
-        alert("El password y la verificación del mismo no coinciden. Intenta de nuevo.")
-    }
+    else if (pswd != "" && verif != "")
+        if (pswd == verif){
+            forma.innerHTML = `<h2 id="aceptado" class="containerMarcado">¡Password nueva confirmada con éxito! (La contraseña coincide con su verificación).</h2>`;
+        }
+        else {
+            inputVerif.insertAdjacentHTML("afterend", `<p class="inputWarning">El password y la verificación del mismo no coinciden. <br>Intenta de nuevo.</p>`);
+            forma.reset();
+        }
 }
 
 enviado.onclick = validar;
+reset.onclick = limpiar;
